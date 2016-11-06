d561a5f9765e584bceea364d90fad7db67d5e91c
SanduRadu <radu.denis.rd@gmail.com>
2016-11-06 21:53:19 +0200

    Add files via upload

---
+++ T1_ex3a_Razvan_Craciunescu.m
@@ -0,0 +1,14 @@
+function [ ma ] = ex3_1( z )
+
+for i=1:+1:10
+   a=randn;
+   b=randn;
+   z(i)=a+b*j;
+end
+
+
+r=real(z);%luam partea reala a numereleor complexe
+ma=mean(r);%facem media aritmetica a partilor reale ale elementelor vectorului
+
+end
+%functia ce are ca parametru de intrare vectorul genera,z si ca parametru de iesire media artmetica
---
+++ T1_ex3b_Razvan_Craciunescu.m
@@ -0,0 +1,14 @@
+function [ p ] = ex3_2( z )
+
+for i=1:+1:10
+   a=randn;
+   b=randn;
+   z(i)=a+b*j;
+end
+
+p=z.*z;%inmultirea element cu element a a numerelor din vectorul z cu ele insele
+
+end
+%functia ce are ca parametru de intrare vectorul generat,z si ca parametru
+%de iesire vectorul ce contine elementele vectorului initial ridicate la
+%patrat
